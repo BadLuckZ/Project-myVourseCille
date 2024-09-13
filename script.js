dcb4c6da87ff1e3041f0af0304653e82b65e8d82
BadLuckZ <thanagorn8802@gmail.com>
2024-09-13 22:44:27 +0700

    Add Register Username and Confirm Password Validation

--- script.js
+++ script.js
@@ -31,30 +31,41 @@ const register_username_error = document.getElementById(
 const register_password_error = document.getElementById(
   "register-password-error-message"
 );
-const register_confirmpassword_error = document.getElementById(
-  "register-confirmpassword-error-message"
+const register_checkpassword_error = document.getElementById(
+  "register-checkpassword-error-message"
 );
 
 register_form.addEventListener("submit", (e) => {
   e.preventDefault();
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const email = register_email.value;
+  let email_error = true;
+
+  // Email Validation
+  if (emailPattern.test(email)) {
+    showSuccess(register_email_error, register_email);
+    email_error = false;
+  } else {
+    showError(register_email_error, register_email, "Invalid Email");
+    email_error = true;
+  }
 
   const username = register_username.value;
+  let username_error = false;
+
+  if (username == "") {
+    showError(register_username_error, register_username, "Invalid Username");
+    username_error = true;
+  } else {
+    showSuccess(register_username_error, register_username);
+    username_error = false;
+  }
 
   const hasUppercase = /[A-Z]/;
   const hasLowercase = /[a-z]/;
   const hasNumber = /\d/;
   const password = register_password.value;
-
-  const checkpassword = register_checkpassword.value;
-
-  // Email Validation
-  if (emailPattern.test(email)) {
-    showSuccess(register_email_error, register_email);
-  } else {
-    showError(register_email_error, register_email, "Invalid Email");
-  }
+  let password_error = true;
 
   // Password Validation
   if (!hasUppercase.test(password)) {
@@ -63,25 +74,53 @@ register_form.addEventListener("submit", (e) => {
       register_password,
       "Password must have at least 1 uppercase letter"
     );
+    password_error = true;
   } else if (!hasLowercase.test(password)) {
     showError(
       register_password_error,
       register_password,
       "Password must have at least 1 lowercase letter"
     );
+    password_error = true;
   } else if (!hasNumber.test(password)) {
     showError(
       register_password_error,
       register_password,
       "Password must have at least 1 number"
     );
+    password_error = true;
   } else if (password.length < 8) {
     showError(
       register_password_error,
       register_password,
       "Password must have at least 8 characters"
     );
+    password_error = true;
   } else {
     showSuccess(register_password_error, register_password);
+    password_error = false;
+  }
+
+  const check_password = register_checkpassword.value;
+  let check_password_error = true;
+
+  // Confirm Password Validation
+  if (check_password == "") {
+    showError(
+      register_checkpassword_error,
+      register_checkpassword,
+      "Invalid Password"
+    );
+    check_password_error = true;
+  } else if (check_password != password) {
+    showError(
+      register_checkpassword_error,
+      register_checkpassword,
+      "Passwords do not match"
+    );
+    check_password_error = true;
+  } else {
+    showSuccess(register_checkpassword_error, register_checkpassword);
+    check_password_error = false;
   }
 });
